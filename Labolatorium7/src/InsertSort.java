d37f75d15ea4e46b251c56bb7c2f5c83712756b6
grudus <jakgruda@gmail.com>
2017-05-10 22:54:50 +0200

    Change implementation of insertion sort
    Now it works unbelievable fast for sorted array

--- Labolatorium7/src/InsertSort.java
+++ Labolatorium7/src/InsertSort.java
@@ -3,12 +3,14 @@ public class InsertSort implements Sortable {
     @Override
     public int[] sort(int[] array) {
         final int len = array.length;
-        for (int i = 1; i < len; i++) {
-            for(int j = i ; j > 0 ; j--){
-                if(array[j] < array[j-1]){
-                   swap(array, j, j-1);
-                }
+        for (int i = 0; i < len; i++) {
+            final int copyNumber = array[i];
+            int j = i;
+            while (j > 0 && copyNumber < array[j-1]) {
+                array[j] = array[j-1];
+                j--;
             }
+            array[j] = copyNumber;
         }
         return array;
     }
