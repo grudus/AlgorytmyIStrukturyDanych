5772eb37aa3cbfea75d611bda3abfdb3049cd610
grudus <jakgruda@gmail.com>
2017-06-04 17:46:08 +0200

    Minor refactor; Saving encoded text to file

--- Laboratorium11/src/Lab11.java
+++ Laboratorium11/src/Lab11.java
@@ -2,6 +2,10 @@ import sentence.Letter;
 import sentence.Sentence;
 import tree.HuffmanTree;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -14,12 +18,39 @@ public class Lab11 {
     private static final String TEXT = "ala i ola.";
 
     public static void main(String[] args) {
-        Queue<Letter> quee = new Sentence(TEXT).characterCountAsQueue();
+        HuffmanTree tree = createTree();
+        tree.printAsTree();
 
-        Queue<HuffmanTree> trees = new PriorityQueue<>(comparing(b -> b.getRoot().getValue()));
+        Map<Character, String> charsToCode = tree.findCode().entrySet()
+                .stream().collect(toMap(e -> e.getKey().getLetter(), Map.Entry::getValue));
 
-        while (!quee.isEmpty()) {
-            trees.add(new HuffmanTree(quee.poll()));
+        tree.findCode().forEach((letter, code) -> System.out.printf("%s|%d|%s\n", letter.getLetter(), letter.getFrequency(), code));
+
+        String encoded = TEXT.chars().mapToObj(i -> charsToCode.get((char) i)).reduce((s1, s2) -> s1 + " " + s2).get();
+        System.out.println(encoded);
+
+        System.out.println(tree.decode(encoded.replaceAll(" ", "")));
+
+        writeToFile(encoded);
+
+
+    }
+
+    private static void writeToFile(String encoded) {
+        String filePath = new File("").getAbsolutePath() + "/text.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(encoded);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static HuffmanTree createTree() {
+        Queue<Letter> letters = new Sentence(TEXT).characterCountAsQueue();
+        Queue<HuffmanTree> trees = new PriorityQueue<>(comparing(tree -> tree.getRoot().getValue()));
+
+        while (!letters.isEmpty()) {
+            trees.add(new HuffmanTree(letters.poll()));
         }
 
         while (trees.size() > 1) {
@@ -30,23 +61,7 @@ public class Lab11 {
             newTree.addRight(second);
             trees.add(newTree);
         }
-
-        HuffmanTree tree = trees.poll();
-
-        tree.printAsTree();
-        Map<Character, String> charsToCode = tree.findCode().entrySet()
-                .stream().collect(toMap(e -> e.getKey().getLetter(), Map.Entry::getValue));
-
-        tree.findCode().forEach((letter, code) -> System.out.printf("%s|%d|%s\n", letter.getLetter(), letter.getFrequency(), code));
-
-
-        String decoded = TEXT.chars().mapToObj(i -> charsToCode.get((char) i)).reduce((s1, s2) -> s1 + " " + s2).get();
-
-        System.out.println(decoded);
-
-
-        System.out.println(tree.decode(decoded.replaceAll(" ", "")));
-
-
+        return trees.poll();
     }
+
 }
